3f6b847dffbed132a9811e9f36c82e2a5de6c33b
alexpimania <alexpimania@gmail.com>
2016-04-21 07:36:34 +1000

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,35 @@
-
+function makeLink() {
+      var email = document.getElementById("email").value;
+      var cc = document.getElementById("cc").value;
+      var bcc = document.getElementById("bcc").value;
+      var subject = document.getElementById("subject").value;
+      var body = document.getElementById("body").value;
+      var link = "mailto:" + email + "?CC=" + cc + "&BCC=" + bcc + "&subject=" + subject + "&body=" + body;
+      document.getElementById("link").href = link;
+      var target = document.getElementById('link');
+      var wrap = document.createElement('div');
+      wrap.appendChild(target.cloneNode(true));
+      document.getElementById("result").value = wrap.innerHTML;
+      var boxlist = ["email", "cc", "bcc", "subject", "body"];
+      for (var i = 0; i < boxlist.length; i++) {
+        document.getElementById(boxlist[i]).value = "";
+      }
+    }
+  window.onload = function() {
+    function updateFeed() 
+    {
+        var feedImage = document.getElementById("feed");
+        feedImage.src = feedImage.src.split("?")[0] + "?currentTime=" + new Date().getTime();
+    }
+    function updateCat()
+    {
+      var catImage = document.getElementById("cat");
+      catImage.src="http://thecatapi.com/api/images/get?format=src&type=gif&currentTime=" + new Date().getTime();
+    }
+    
+    updateFeed();
+    updateCat();
+    
+    setInterval(updateCat, 10000);
+    setInterval(updateFeed, 20000);
+  }
